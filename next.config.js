ed5a6104135b02f089b8a6678ad19c893afe2015
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2024-02-17 08:27:52 +0330

    style: remove appName from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
 const createNextIntlPlugin = require('next-intl/plugin')
 const path = require('path')
 const withNextIntl = createNextIntlPlugin()
-const appName = 'nextBoilderplate'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   publicRuntimeConfig: {
     apiBaseUrl: process.env.API_URL,
     siteUri: process.env.SITE_URI,
-    appName
-  },
-
-  env: {
-    APP_NAME: appName
+    appName: process.env.APP_NAME
   },
 
   sassOptions: {
