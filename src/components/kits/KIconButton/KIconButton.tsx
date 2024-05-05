acffca06d9183086e781618d906f565ad82864ec
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2024-05-05 10:14:36 +0330

    feat: add tooltip to KiconBtn

--- src/components/kits/KIconButton/KIconButton.tsx
+++ src/components/kits/KIconButton/KIconButton.tsx
@@ -1,21 +1,27 @@
 import IconButton, { type IconButtonOwnProps } from '@mui/material/IconButton'
 import CachedOutlinedIcon from '@mui/icons-material/CachedOutlined'
-
+import Tooltip, { type TooltipProps } from '@mui/material/Tooltip'
 interface ButtonProps extends IconButtonOwnProps {
   loading?: boolean
   disabled?: boolean
+  toolTipTitle?: string
+  tooltipProps?: TooltipProps
   [key: string]: any
 }
 
 export function KIconButton({
   loading = false,
   disabled = false,
+  toolTipTitle = '',
+  tooltipProps,
   children,
   ...props
 }: Readonly<ButtonProps>) {
   return (
-    <IconButton disabled={disabled || loading} {...props}>
-      {loading ? <CachedOutlinedIcon className="rotatez-animation" /> : children}
-    </IconButton>
+    <Tooltip title={toolTipTitle} {...tooltipProps}>
+      <IconButton disabled={disabled || loading} {...props}>
+        {loading ? <CachedOutlinedIcon className="rotatez-animation" /> : children}
+      </IconButton>
+    </Tooltip>
   )
 }
